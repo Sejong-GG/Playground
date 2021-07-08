acfafd26702cdcc46fc48e95a65b1f23b5bef259
riyenas0925 <riyenas0925@gmail.com>
2021-07-08 15:47:40 +0900

    feat : 채팅방 생성
    * /main 네임스페이스에 접속된 모든 클라이언트에 새로운 방이 생성되면 emit
    * 모든 html 페이지가 layout에서 확장
    * 새로운 클라이언트가 접속하면 랜덤한 색상값을 발급받고 session에 color라는 이름으로 저장

--- room/app.js
+++ room/app.js
@@ -4,6 +4,7 @@ const morgan = require('morgan');
 const cookieParser = require('cookie-parser');
 const session = require('express-session');
 const nunjucks = require('nunjucks');
+const ColorHash = require('color-hash');
 const dotenv = require('dotenv');
 
 dotenv.config();
@@ -20,12 +21,7 @@ nunjucks.configure('views', {
 });
 connect();
 
-app.use(morgan('dev'));
-app.use(express.static(path.join(__dirname, 'public')));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser(process.env.COOKIE_SECRET));
-app.use(session({
+const sessionMiddleware = session({
   resave: false,
   saveUninitialized: false,
   secret: process.env.COOKIE_SECRET,
@@ -33,7 +29,22 @@ app.use(session({
     httpOnly: true,
     secure: false,
   },
-}));
+});
+app.use(morgan('dev'));
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser(process.env.COOKIE_SECRET));
+app.use(sessionMiddleware);
+
+app.use((req, res, next) => {
+  if(!req.session.color) {
+    const colorHash = new ColorHash();
+    req.session.color = colorHash.hex(req.sessionID);
+    console.log(req.session.color)
+  }
+  next();
+})
 
 app.use('/', indexRouter);
 
@@ -54,4 +65,4 @@ const server = app.listen(app.get('port'), () => {
   console.log(app.get('port'), '번 포트에서 대기중');
 });
 
-webSocket(server);
+webSocket(server, app, sessionMiddleware);
--- room/routes/index.js
+++ room/routes/index.js
@@ -1,9 +1,39 @@
 const express = require('express');
 
 const router = express.Router();
+const Room = require('../schemas/room');
+const Chat = require('../schemas/chat');
 
-router.get('/', (req, res) => {
-  res.render('index');
+// / 경로 접속
+router.get('/', async (req, res, next) => {
+  try {
+    const rooms = await Room.find({});
+    res.render('main', { rooms, title: 'GIF 채팅방' });
+  } catch (error) {
+    console.error(error);
+    next(error);
+  }
+});
+
+router.get('/room', (req, res) => {
+  res.render('room', { title: 'GIF 채팅방 생성' });
+});
+
+router.post('/room', async (req, res, next) => {
+  try {
+    const newRoom = await Room.create({
+      title: req.body.title,
+      max: req.body.max,
+      owner: req.session.color,
+      password: req.body.password,
+    });
+    const io = req.app.get('io');
+    io.of('/room').emit('newRoom', newRoom);
+    //res.redirect(`/room/${newRoom._id}?password=${req.body.password}`);
+  } catch (error) {
+    console.error(error);
+    next(error);
+  }
 });
 
 module.exports = router;
