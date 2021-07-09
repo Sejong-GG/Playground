d4731a30ede5e8d54dbc953e6994bdb42f680ae5
riyenas0925 <riyenas0925@gmail.com>
2021-07-09 19:58:37 +0900

    feat : 채팅방 접속 기능 구현
    * chat 네임스페이스 정의
    * 채팅방 입장 제한 구현

--- room/routes/index.js
+++ room/routes/index.js
@@ -4,7 +4,7 @@ const router = express.Router();
 const Room = require('../schemas/room');
 const Chat = require('../schemas/chat');
 
-// / 경로 접속
+// / -> get
 router.get('/', async (req, res, next) => {
   try {
     const rooms = await Room.find({});
@@ -15,10 +15,12 @@ router.get('/', async (req, res, next) => {
   }
 });
 
+// /room -> get
 router.get('/room', (req, res) => {
   res.render('room', { title: 'GIF 채팅방 생성' });
 });
 
+// /room -> post
 router.post('/room', async (req, res, next) => {
   try {
     const newRoom = await Room.create({
@@ -29,10 +31,45 @@ router.post('/room', async (req, res, next) => {
     });
     const io = req.app.get('io');
     io.of('/room').emit('newRoom', newRoom);
-    //res.redirect(`/room/${newRoom._id}?password=${req.body.password}`);
+    res.redirect(`/room/${newRoom._id}?password=${req.body.password}`);
   } catch (error) {
     console.error(error);
     next(error);
+  }
+});
+
+// /room/{id}?password={pw} -> 
+router.get('/room/:id', async (req, res, next) => {
+  try {
+    const room = await Room.findOne({ _id: req.params.id });  // id가 3인 채팅방을 조회해라
+    const io = req.app.get('io');                             //  
+    // 방이 있는지
+    if (!room) {
+      return res.redirect('/?error=존재하지 않는 방입니다.');
+    }
+    // 비밀번호 맞는지
+    if (room.password && room.password !== req.query.password) {
+      return res.redirect('/?error=비밀번호가 틀렸습니다.');
+    }
+    // 
+    const { rooms } = io.of('/chat').adapter;
+
+    console.log(rooms);
+
+    // 허용인원 넘는지
+    if (rooms && rooms[req.params.id] && room.max <= rooms[req.params.id].length) {
+      return res.redirect('/?error=허용 인원이 초과하였습니다.');
+    }
+    
+    return res.render('chat', {
+      room,
+      title: room.title,
+      chats: [],
+      user: req.session.color,
+    });
+  } catch (error) {
+    console.error(error);
+    return next(error);
   }
 });
 
