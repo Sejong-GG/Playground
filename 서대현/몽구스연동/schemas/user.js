6ddb1c844667c7cbcb473406d15ac421020f3f8e
SeoDaeHyun <sdh4588@gmail.com>
2021-07-10 18:28:11 +0900

    서대현 : 실시간 채팅 따라치기
    * socket 실시간 데이터 전송 이해
    * 몽고디비 이해 필요

---
+++ 서대현/몽구스연동/schemas/user.js
@@ -0,0 +1,21 @@
+const mongoose = require('mongoose');
+
+const { Schema } = mongoose;
+const userSchema = new Schema({
+  name: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  age: {
+    type: Number,
+    required: true,
+  },
+  comment: String,
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+module.exports = mongoose.model('User', userSchema);
